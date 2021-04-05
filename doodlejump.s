35e7af4a16ecee8cdf118222a6ba4d8e618f42d6
Jess Anderson <47649312+ander710@users.noreply.github.com>
2021-04-05 16:52:13 -0400

    Implemented draw sky and draw pipes

--- doodlejump.s
+++ doodlejump.s
@@ -8,7 +8,7 @@
 # - Base Address for Display: 0x10008000 ($gp)
 #
 .data
-	displayAddress:	.word	0x10008000
+	displayAddress:	.word 0x10008000
 	pipeLength: .word 4
 
 	# Colors for painting on the screen
@@ -26,7 +26,7 @@ main:
 paintSkyLoopInit:
 	lw $t0, displayAddress	# $t0 stores the base address for display
 	lw $t1, sky				# $t1 stores the blue colour code
-	addi $t2, $zero, 65536
+	addi $t2, $zero, 4096
 	addi $t2, $t2, 0x10008000
 	j paintSkyLoop
 
@@ -46,7 +46,7 @@ paintPipes:
 	syscall
 	
 	lw $t0, displayAddress
-	
+	 
 	sub $t3, $t3, $t4 
 	add $t0, $t0, $t3
 	
@@ -56,7 +56,7 @@ paintPipes:
 	
 	add $t0, $t0, $t2	
 	
-	lw $t1, sky
+	lw $t1, pipe
 	li $t6, 0
 	li $t7, 7
 	
@@ -69,6 +69,8 @@ paintPipes:
 	li $t4, 1024
 	li $t5, 1920
 	ble $t3, $t5, Exit
+	
+
 	j paintPipes
 
 addBird:
@@ -79,19 +81,3 @@ addBird:
 Exit:
 	li $v0, 10 # terminate the program gracefully
 	syscall
-
-
-
-#	lw $t0, displayAddress	# $t0 stores the base address for display
-#	li $t1, 0xff0000	# $t1 stores the red colour code
-#	li $t2, 0x00ff00	# $t2 stores the green colour code
-#	li $t3, 0x0000ff	# $t3 stores the blue colour code
-	
-#	sw $t1, 0($t0)	 # paint the first (top-left) unit red. 
-#	sw $t2, 4($t0)	 # paint the second unit on the first row green. Why $t0+4?
-#	sw $t3, 128($t0) # paint the first unit on the second row blue. Why +128?
-#	sw $t2, 256($t0)
-
-
-
-
